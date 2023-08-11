777b4a0d901a476f77d51dbabc90a1b5f921aa98
arshpreetsingh3 <38527324+arshpreetsingh3@users.noreply.github.com>
2023-08-10 20:24:24 -0400

    Changed file name

--- check_gpu.py
+++ check_gpu.py
@@ -11,5 +11,5 @@ from IPython.display import display, Image
 print(torch.cuda.is_available())
 
 
-yolo task=detect mode=train model=yolov8x.pt data=data.yaml epochs=25 imgsz= 320 plots=True
-yolo task=detect mode=val model=runs/detect/train/weights/best.pt data=data.yaml
+# yolo task=detect mode=train model=yolov8x.pt data=data.yaml epochs=25 imgsz= 320 plots=True
+# yolo task=detect mode=val model=runs/detect/train/weights/best.pt data=data.yaml
